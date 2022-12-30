c0e3b4240cfccee793d11c3cfaebd9e0afb91d16
Avi Deep <deepa814@newschool.edu>
2022-12-30 19:44:15 +0530

    Adding entry 2 and 3

---
+++ homework/musuem/entries/entry3/script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+$("#line1").fadeOut(2000);
+setTimeout(function(){$("#line2").fadeOut(2000)}, 2000);
+setTimeout(function(){$("#line3").fadeOut(2000)}, 4000);  
+setTimeout(function(){$("#line4").fadeOut(2000)}, 5000);  
+setTimeout(function(){$("#line5").fadeOut(2000)}, 6000);  
+setTimeout(function(){$("#line6").fadeOut(2000)}, 8000);  
+setTimeout(function(){$("#line7").fadeOut(2000)}, 10000);  
+setTimeout(function(){$("#line8").fadeOut(2000)}, 12000);  
+setTimeout(function(){$("#line9").fadeOut(2000)}, 14000);  
+setTimeout(function(){$("#line10").fadeOut(2000)},16000);  
+setTimeout(function(){$("#line11").fadeOut(2000)},18000);
+setTimeout(function(){$("#line12").fadeOut(2000)},20000); 
+setTimeout(function(){$("#line13").fadeOut(2000)},22000);  
+setTimeout(function(){$("#line14").fadeOut(2000)},24000);  
+setTimeout(function(){$("#line15").fadeOut(2000)},26000);  
+setTimeout(function(){$("#line16").fadeOut(2000)},28000);  
+}) 
+
+
